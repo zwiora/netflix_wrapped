ad5d98ac77f46fec9893f6c33daf77082fbbf9aa
Zofia Wiora <zofia.wiora@gmail.com>
2025-06-19 14:38:07 +0200

    Basic API server

--- API/main.go
+++ API/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type entity struct {
+	ID      string `json:"id"`
+	Content string `json:"content"`
+}
+
+var entities = []entity{
+	{ID: "1", Content: "Test1"},
+	{ID: "2", Content: "Test2"},
+	{ID: "3", Content: "Test3"},
+}
+
+func getEntities(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, entities)
+}
 
 func main() {
-	fmt.Println("test")
+	fmt.Println("Starting application")
+	port := "8080"
+
+	router := gin.Default()
+	router.GET("/test", getEntities)
+	router.Run("localhost:" + port)
+
+	fmt.Println("Server running on port " + port)
 }
