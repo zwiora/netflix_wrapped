6063652571fe4a1d4a5cfbbf207f7db09acd36e6
Jakub Włodarski <jaecob01@gmail.com>
2025-06-20 11:58:42 +0200

    Revert changes in home.routes.js

--- application/routes/home.routes.js
+++ application/routes/home.routes.js
@@ -3,7 +3,6 @@ const _ = require('express-session');
 const router = express.Router();
 const formidable = require('formidable');
 const path = require('path');
-const fs = require('fs');
 
 
 router.get('/', function(_, res, next) {
@@ -19,30 +18,19 @@ router.get('/waiting', (_, res) => {
 });
 
 router.post('/upload', (req, res) => {
-  const form = new formidable.IncomingForm({
+  let form = new formidable.IncomingForm({
     uploadDir: path.join(__dirname, '../../uploads'),
     keepExtensions: true
   });
-
   form.parse(req, function (err, fields, files) {
     if (err || !files.netflixData) {
-      return res.status(400).json({ success: false, error: 'No file uploaded.' });
+      return res.status(400).send('No file uploaded.');
     }
-
-    const uploadedFile = Array.isArray(files.netflixData)
-      ? files.netflixData[0]
-      : files.netflixData;
-
-    // Save it temporarily (could store path in session, too)
-    req.session.uploadedFile = uploadedFile;
-
-    return res.status(200).json({
-      success: true,
-      filename: uploadedFile.originalFilename
-    });
-  });
+    const uploadedFile = Array.isArray(files.netflixData) ? files.netflixData[0] : files.netflixData;
+    req.session.reportId = uploadedFile.newFilename;
+    res.redirect('/waiting');
+  })
 });
-
 
 router.get('/report', (req, res) => {
   if (!req.session.reportId) {
@@ -51,19 +39,8 @@ router.get('/report', (req, res) => {
   }
   res.render('report', {
     title: "Netflix Wrapped - report",
-    report: req.session.report,
-    reportId: req.session.reportId
+    report: req.session.report
   });
 });
-
-router.get('/report/:id', (req, res) => {
-  try {
-    req.session.reportId = req.params.id;
-    req.session.report = JSON.parse(fs.readFileSync(path.join(__dirname, '../../reports', req.session.reportId), 'utf8'));
-    res.redirect('/report');
-  } catch (_) {
-    res.redirect('/');
-  }
-})
 
 module.exports = router;
